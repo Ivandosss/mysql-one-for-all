9827a14c0eb4573981003c6ad024e391a7c4e3f1
Linux <ivan@pop-os.localdomain>
2021-11-03 12:13:23 -0300

    Requisito 6

---
+++ desafio6.sql
@@ -0,0 +1,7 @@
+CREATE VIEW faturamento_atual AS
+SELECT ROUND(MIN(p.valor_plano),2) AS 'faturamento_minimo',
+ROUND(MAX(p.valor_plano),2) AS 'faturamento_maximo',
+ROUND(SUM(p.valor_plano) / COUNT(u.usuario_id),2) AS 'faturamento_medio',
+ROUND(SUM(p.valor_plano),2) AS 'fatueamento_total'
+FROM SpotifyClone.planos AS p
+JOIN SpotifyClone.usuarios AS u ON p.plano_id = u.plano_id; 
