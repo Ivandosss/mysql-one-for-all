073b8e27b915d5d4bebb3179b93da7a4ab2b0614
Linux <ivan@pop-os.localdomain>
2021-11-03 11:19:15 -0300

    Requisito 4 correcao

--- desafio4.sql
+++ desafio4.sql
@@ -3,5 +3,6 @@ SELECT a.artista AS artista,
 COUNT(s.usuario_id) AS seguidores
 FROM SpotifyClone.artistas AS a 
 JOIN SpotifyClone.seguindo_artistas AS s ON s.artista_id = a.artista_id
+GROUP BY artista
 ORDER BY seguidores DESC, artista ASC LIMIT 3;
   
