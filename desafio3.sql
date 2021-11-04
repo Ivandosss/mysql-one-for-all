996443b641fe0d605c282194683c562c73c17df5
Linux <ivan@pop-os.localdomain>
2021-11-04 09:54:08 -0300

    desafio 3

--- desafio3.sql
+++ desafio3.sql
@@ -1,6 +1,7 @@
+DROP VIEW IF EXISTS historico_reproducao_usuarios;
 CREATE VIEW historico_reproducao_usuarios AS 
 SELECT u.usuario AS usuario, m.musica AS nome
 FROM SpotifyClone.usuarios AS u
 JOIN SpotifyClone.historico AS h ON u.usuario_id = h.usuario_id
 JOIN SpotifyClone.musicas AS m ON m.musica_id = h.musica_id
-ORDER BY usuario, nome;  
+ORDER BY usuario ASC, nome ASC;  
