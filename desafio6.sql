77130cf0285a8199a12d0b42a8e07da221c92d88
Linux <ivan@pop-os.localdomain>
2021-11-04 16:00:46 -0300

    desafio 7

--- desafio6.sql
+++ desafio6.sql
@@ -4,5 +4,5 @@ ROUND(MIN(p.price),2) AS 'faturamento_minimo',
 ROUND(MAX(p.price),2) AS 'faturamento_maximo',
 ROUND(AVG(p.price),2) AS 'faturamento_medio',
 ROUND(SUM(p.price),2) AS 'faturamento_total'
-FROM SpotifyClone.user AS u
-INNER JOIN SpotifyClone.plan AS p ON p.plan_id = u.plan_id; 
+FROM SpotifyClone.`user` AS u
+INNER JOIN SpotifyClone.plan AS p ON p.plan_id = u.plan_id;
