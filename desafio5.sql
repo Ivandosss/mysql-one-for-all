62669eb0e0f07835954d6c154fe55f7ade382afc
Linux <ivan@pop-os.localdomain>
2021-11-04 11:08:15 -0300

    desafio 5

--- desafio5.sql
+++ desafio5.sql
@@ -1,8 +1,8 @@
 CREATE VIEW top_2_hits_do_momento AS 
 SELECT (SELECT musica FROM SpotifyClone.musicas AS m
-WHERE m.musica_id = h.musica_id) AS cancoes,
+WHERE m.musica_id = h.musica_id) AS cancao,
 (SELECT COUNT(*) FROM SpotifyClone.historico AS h 
 WHERE musica_id = h.musica_id) AS reproducoes
 FROM SpotifyClone.historico AS h
 GROUP BY h.musica_id
-ORDER BY reproducoes DESC, cancoes ASC LIMIT 2; 
+ORDER BY reproducoes DESC, cancao ASC LIMIT 2; 
