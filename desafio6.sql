6f184d38f1aa3af6ecbac0b8bb7799513051961e
Linux <ivan@pop-os.localdomain>
2021-11-04 15:26:21 -0300

    modificacoes desafio 6

--- desafio6.sql
+++ desafio6.sql
@@ -2,7 +2,7 @@ CREATE VIEW faturamento_atual AS
 SELECT
 ROUND(MIN(p.price),2) AS 'faturamento_minimo', 
 ROUND(MAX(p.price),2) AS 'faturamento_maximo',
-ROUND(AVG(p.price)) AS 'faturamento_medio',
+ROUND(AVG(p.price),2) AS 'faturamento_medio',
 ROUND(SUM(p.price),2) AS 'faturamento_total'
 FROM SpotifyClone.user AS u
 INNER JOIN SpotifyClone.plan AS p ON p.plan_id = u.plan_id; 
