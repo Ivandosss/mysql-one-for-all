1ae75e2d1475ed5f4506bb12d6221c006e53f50e
Linux <ivan@pop-os.localdomain>
2021-11-04 19:21:27 -0300

    ajuste desafio 7 e 9

--- desafio9.sql
+++ desafio9.sql
@@ -2,9 +2,9 @@ DELIMITER $$
 CREATE PROCEDURE albuns_do_artista (IN artist_name varchar(40))
 BEGIN
 SELECT a.name AS artista, al.title AS album
-FROM SpotifyClone.album AS al 
-INNER JOIN SpotifyClone.artist AS a ON a.artist_id = al.artist_id
-WHERE a.name = artist_name
+FROM album AS al 
+INNER JOIN artist AS a ON a.artist_id = al.artist_id
+WHERE a.`name` = artist_name
 ORDER BY album;
 END $$
 DELIMITER ;
