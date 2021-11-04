82a96ff390f271012191c0ae37a5f4b5e8f20724
Linux <ivan@pop-os.localdomain>
2021-11-04 12:10:26 -0300

    desafio 5

--- desafio5.sql
+++ desafio5.sql
@@ -1,8 +1,8 @@
 DROP VIEW IF EXISTS top_2_hits_do_momento; 
 CREATE VIEW top_2_hits_do_momento AS 
-SELECT m.musica AS cancoes, 
+SELECT m.musica AS cancao, 
 COUNT(h.musica_id) AS reproducoes
 FROM SpotifyClone.musicas AS m 
 JOIN SpotifyClone.historico AS h ON m.musica_id = h.musica_id
 GROUP BY musica
-ORDER BY reproducoes DESC, cancoes ASC LIMIT 2;
+ORDER BY reproducoes DESC, cancao ASC LIMIT 2;
