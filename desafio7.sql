065443e4f8341fd40911c3daa46963c2ea7249bd
Linux <ivan@pop-os.localdomain>
2021-11-04 14:01:17 -0300

    modificacoes desafio 7

---
+++ desafio7.sql
@@ -0,0 +1,8 @@
+CREATE VIEW perfil_artistas AS 
+SELECT a.name AS artista,
+al.title AS album,
+(SELECT COUNT(*) FROM SpotifyClone.followers AS f 
+WHERE a.artist_id = f.artist_id) AS seguidores
+FROM SpotifyClone.album AS al
+INNER JOIN SpotifyClone.artist AS a ON a.artist_id = al.artist_id
+ORDER BY seguidores DESC, artista ASC, album ASC;  
