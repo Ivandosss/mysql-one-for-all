65aa35320698201d3a7c5c092178ef65e31c6126
Linux <ivan@pop-os.localdomain>
2021-11-04 13:40:24 -0300

    modificacoes desafio 5 e 6

--- desafio5.sql
+++ desafio5.sql
@@ -1,8 +1,7 @@
-DROP VIEW IF EXISTS top_2_hits_do_momento; 
 CREATE VIEW top_2_hits_do_momento AS 
-SELECT m.musica AS cancao, 
+SELECT s.title AS cancao, 
 COUNT(*) AS reproducoes
-FROM SpotifyClone.historico AS h 
-INNER JOIN SpotifyClone.musicas AS m ON m.musica_id = h.musica_id
-GROUP BY musica
+FROM SpotifyClone.history AS h 
+INNER JOIN SpotifyClone.song AS s ON s.song_id = h.song_id
+GROUP BY title
 ORDER BY reproducoes DESC, cancao ASC LIMIT 2;
