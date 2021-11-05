a177f170303caef88d682ebf7c13fe8f915ee003
Linux <ivan@pop-os.localdomain>
2021-11-04 21:22:22 -0300

    ajuste desafio 7 e 9

--- desafio7.sql
+++ desafio7.sql
@@ -1,7 +1,7 @@
 CREATE VIEW perfil_artistas AS 
-SELECT a.name AS artista,
-al.title AS album,
-(SELECT COUNT(*) FROM followers AS f WHERE artist_id = a.artist_id) AS seguidores
+SELECT a.name AS `artista`,
+al.title AS `album`,
+(SELECT COUNT(*) FROM followers AS f WHERE artist_id = a.artist_id) AS `seguidores`
 FROM album AS al
 INNER JOIN artist AS a ON a.artist_id = al.artist_id
-ORDER BY seguidores DESC, artista ASC, album ASC;  
+ORDER BY `seguidores` DESC, `artista` ASC, `album` ASC;  
--- desafio9.sql
+++ desafio9.sql
@@ -1,10 +1,10 @@
 DELIMITER $$
 CREATE PROCEDURE albuns_do_artista (IN artist_name varchar(40))
 BEGIN
-SELECT a.`name` AS artista, al.title AS album
+SELECT a.`name` AS `artista`, al.title AS `album`
 FROM album AS al 
 INNER JOIN artist AS a ON a.artist_id = al.artist_id
 WHERE a.`name` = artist_name
-ORDER BY album;
+ORDER BY `album`;
 END $$
 DELIMITER ;
