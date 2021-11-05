92f0660a68c4d439b50136c13f9729e8a4aafaa6
Linux <ivan@pop-os.localdomain>
2021-11-04 21:16:07 -0300

    ajuste desafio 9

--- desafio9.sql
+++ desafio9.sql
@@ -1,7 +1,7 @@
 DELIMITER $$
 CREATE PROCEDURE albuns_do_artista (IN artist_name varchar(40))
 BEGIN
-SELECT a.name AS artista, al.title AS album
+SELECT a.`name` AS artista, al.title AS album
 FROM album AS al 
 INNER JOIN artist AS a ON a.artist_id = al.artist_id
 WHERE a.`name` = artist_name
