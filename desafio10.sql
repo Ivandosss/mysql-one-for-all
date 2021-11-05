b3a25529067073d7ee731b3b579975d05da3b58c
Linux <ivan@pop-os.localdomain>
2021-11-04 22:31:16 -0300

    desafio 10

--- desafio10.sql
+++ desafio10.sql
@@ -1,1 +1,16 @@
-CREATE VIEW 
+DELIMITER $$
+
+CREATE FUNCTION quantidade_musicas_no_historico (userId INT)
+RETURNS INT READS SQL DATA 
+BEGIN
+
+DECLARE quantity INT;
+
+SELECT COUNT(*) INTO quantity FROM `history`
+WHERE user_id = userId;
+
+RETURN quantity;
+ 
+END $$
+
+DELIMITER ;
