3991ff051d07055debd2a6c9a26a69c9d4f8240a
Linux <ivan@pop-os.localdomain>
2021-11-04 21:58:33 -0300

    ajuste desafio 7

--- desafio7.sql
+++ desafio7.sql
@@ -1,7 +1,9 @@
 CREATE VIEW perfil_artistas AS 
-SELECT a.name AS `artista`,
-al.title AS `album`,
-(SELECT COUNT(*) FROM followers AS f WHERE artist_id = a.artist_id) AS `seguidores`
-FROM album AS al
-INNER JOIN artist AS a ON a.artist_id = al.artist_id
-ORDER BY `seguidores` DESC, `artista` ASC, `album` ASC;  
+SELECT a.name AS artista,
+al.title AS album,
+COUNT(*) AS seguidores
+FROM artist AS a
+JOIN album AS al ON a.artist_id = al.artist_id
+JOIN followers AS f ON f.artist_id = a.artist_id
+GROUP BY album, artista
+ORDER BY seguidores DESC, artista ASC, album ASC;  
