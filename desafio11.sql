6570ab23ebc4f31db4568182cafb3147ebb7b953
Linux <ivan@pop-os.localdomain>
2021-11-04 23:11:56 -0300

    desafio 11

--- desafio11.sql
+++ desafio11.sql
@@ -2,7 +2,6 @@ CREATE VIEW cancoes_premium AS
 SELECT s.title AS nome,
 COUNT(h.song_id) AS reproducoes FROM `history` AS h
 INNER JOIN song AS s ON s.song_id = h.song_id 
-WHERE h.user_id IN (SELECT user_id FROM `user` 
-WHERE plan_id IN (2,3))
+WHERE h.user_id IN (SELECT user_id FROM `user` WHERE plan_id IN (2,3))
 GROUP BY nome
 ORDER BY nome;
