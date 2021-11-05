d580b43634c9e0a35b14206f3fb2c86a0df649a6
Linux <ivan@pop-os.localdomain>
2021-11-04 22:11:25 -0300

    ajuste agora vai

--- desafio7.sql
+++ desafio7.sql
@@ -1,9 +1,9 @@
-CREATE VIEW perfil_artistas AS 
+CREATE VIEW SpotifyClone.perfil_artistas AS 
 SELECT a.name AS artista,
 al.title AS album,
 COUNT(*) AS seguidores
-FROM artist AS a
-JOIN album AS al ON a.artist_id = al.artist_id
-JOIN followers AS f ON f.artist_id = a.artist_id
+FROM SpotifyClone.artist AS a
+JOIN SpotifyClone.album AS al ON a.artist_id = al.artist_id
+JOIN SpotifyClone.followers AS f ON f.artist_id = a.artist_id
 GROUP BY album, artista
 ORDER BY seguidores DESC, artista ASC, album ASC;  
--- desafio9.sql
+++ desafio9.sql
@@ -1,10 +1,12 @@
 DELIMITER $$
-CREATE PROCEDURE albuns_do_artista (IN artist_name varchar(100))
+
+CREATE PROCEDURE SpotifyClone.albuns_do_artista (IN artist_name varchar(100))
 BEGIN
 SELECT a.name AS artista, al.title AS album
-FROM album AS al 
-JOIN artist AS a ON a.artist_id = al.artist_id
+FROM SpotifyClone.album AS al 
+JOIN SpotifyClone.artist AS a ON a.artist_id = al.artist_id
 WHERE a.name = artist_name
 ORDER BY album;
 END $$
+
 DELIMITER ;
